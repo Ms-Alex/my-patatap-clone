a107f898ffb257988c894221e16198d4e82abb92
Ms-Alex <ms.hernandeza1@gmail.com>
2019-03-17 23:23:50 -0400

    add paperscript file, update css for project main page, .gitignore to include package-lock, create test line for js file, and update html index page to include external files

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,11 @@
-alert('connected');
 
-// jQuery connected
-if (jQuery) {
-    console.log("$ is here.")
-}
+// Create a Paper.js Path to draw a line into it:
+var path = new Path();
+// Give the stroke a color
+path.strokeColor = 'yellow';
+var start = new Point(100, 100);
+// Move to start and draw a line from there
+path.moveTo(start);
+// Note the plus operator on Point objects.
+// PaperScript does that for us.
+path.lineTo(start + [100, -50]);
